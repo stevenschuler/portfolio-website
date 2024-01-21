79f570a2dfb6f7c6aab0106e4035b081af0267f9
Steven Schuler <stevenschuler5983@yahoo.ca>
2024-01-20 17:14:35 -0800

    Finished part 3 of the tutorial. Used image plugin and pulled in images from both the web and local.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import * as React from "react"
 import { Link } from 'gatsby'
 import Layout from './Layout'
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
     <main>
       <Layout pageTitle="Main Page">
         <p> This is my home page!</p>
+        <StaticImage
+        src="https://www.nawpic.com/media/2020/cute-cat-nawpic-7.jpg"
+      />
       </Layout>
     </main>
   )
