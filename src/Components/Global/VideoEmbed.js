954e6f106c83ff17fbf6d74c69d0926eba4bf06a
Steven Schuler <stevenschuler5983@yahoo.ca>
2024-03-09 18:52:09 -0800

    Added lazy loading for youtube videos.

--- src/Components/Global/VideoEmbed.js
+++ src/Components/Global/VideoEmbed.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
+import LazyLoad from "vanilla-lazyload";
 
 const VideoEmbed = ({link}) => {
+  useEffect(() => {
+    const lazyLoadInstance = new LazyLoad({
+      elements_selector: '.lazy',
+    });
+    return () => lazyLoadInstance.destroy();
+  }, []);
+
   return (
     <div>
-        <iframe width="560" height="315" 
-        src={link} frameBorder="0" 
+        <iframe 
+        className="lazy"
+        width="560" height="315" 
+        data-src={link} frameBorder="0" 
         allowFullScreen
         title="Video Player">
         </iframe>
